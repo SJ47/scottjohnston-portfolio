6217ae8b29813bc7d2eb073badd719ecff6dff73
Scott Johnston <scott@umachan.co.uk>
2021-03-29 18:46:24 +0100

    navbar highlighted active links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,56 @@
 import React from 'react'
+import { NavHashLink } from 'react-router-hash-link';
 
 const NavBar = () => {
+
+    const activeStyle = {
+        borderBottom: "5px solid #e4c619",
+        // borderRight: "5px solid #e4c619",
+        // color: "grey",
+        // borderRadius: "50%",
+        // padding: ".3em .8em",
+    }
+
     return (
 
         <div className="navigation">
 
             <div className="navigation-sub">
-                {/* <Switch>
-                    <Route path="/endangered-animals">
-                        <EndangeredAnimals />
-                    </Route>
-                </Switch>
-                <Link to="/endangered-animals" className="item">Endangered</Link> */}
+                <NavHashLink smooth to=
+                    "/#services"
+                    className="item"
+                    activeClassName="activeRoute"
+                    activeStyle={activeStyle}
+                >
+                    My Services
+                </NavHashLink>
 
-                <a href="/#services" className="item">My Services</a>
-                <a href="/#about" className="item">About Me</a>
-                <a href="/#projects" className="item">My Projects</a>
-                <a href="#footer" className="item">Reach Out</a>
+                <NavHashLink smooth to=
+                    "/#about"
+                    className="item"
+                    activeClassName="activeRoute"
+                    activeStyle={activeStyle}
+                >
+                    About Me
+                </NavHashLink>
+
+                <NavHashLink smooth to=
+                    "/#projects"
+                    className="item"
+                    activeClassName="activeRoute"
+                    activeStyle={activeStyle}
+                >
+                    My Projects
+                </NavHashLink>
+
+                <NavHashLink smooth to=
+                    "/#footer"
+                    className="item"
+                    activeClassName="activeRoute"
+                    activeStyle={activeStyle}
+                >
+                    Reach Out
+                </NavHashLink>
             </div>
         </div>
     );
