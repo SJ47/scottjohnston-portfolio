aab52cbbc72d5059f6f1e4d2b26b758567981bb0
Scott Johnston <scott@umachan.co.uk>
2021-05-05 22:51:18 +0100

    Navlinks not responding in project pages Fixes #6

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
-// import { NavHashLink as Link } from 'react-router-hash-link';
-import { Link } from "react-scroll"
+import { NavHashLink as Link } from 'react-router-hash-link';
+// import { NavLink as Link } from 'react-router-dom';
+
+// import { Link } from "react-scroll"
 
 const NavBar = () => {
 
@@ -12,51 +14,69 @@ const NavBar = () => {
     //     padding: ".3em .8em",
     // }
 
+    const scrollWithOffset = (el, offset) => {
+        const elementPosition = el.offsetTop - offset;
+        window.scroll({
+            top: elementPosition,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
     return (
 
         <div className="navigation">
 
             <div className="navigation-sub">
                 <Link
-                    activeClass="active"
-                    spy={true}
+                    // activeClass="active"
+                    spy="true"
                     smooth={true}
                     offset={-60}
                     duration={1000}
-                    to="services"
+                    // to="/services"
+                    to="/#services"
+                    scroll={el => scrollWithOffset(el, 78)}
                 >
                     My Services
                 </Link>
 
                 <Link
-                    activeClass="active"
-                    spy={true}
+                    // activeClass="active"
+                    spy="true"
                     smooth={true}
                     offset={-60}
                     duration={1000}
-                    to="about"
+                    // to="about"
+                    to="/#about"
+                    scroll={el => scrollWithOffset(el, 78)}
+
                 >
                     About Me
                 </Link>
 
                 <Link
-                    activeClass="active"
-                    spy={true}
+                    // activeClass="active"
+                    spy="true"
                     smooth={true}
                     offset={-55}
                     duration={1000}
-                    to="projects"
+                    // to="projects"
+                    to="/#projects"
+                    scroll={el => scrollWithOffset(el, 78)}
                 >
                     My Projects
                 </Link>
 
                 <Link
-                    activeClass="active"
-                    spy={true}
+                    // activeClass="active"
+                    spy="true"
                     smooth={true}
                     offset={-55}
                     duration={1000}
-                    to="footer"
+                    // to="footer"
+                    to="/#footer"
+                    scroll={el => scrollWithOffset(el, 78)}
                 >
                     Reach Out
                 </Link>
