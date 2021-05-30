886214bd0b11c5040e9934dde9c14e1d39a23455
Scott Johnston <scott@umachan.co.uk>
2021-05-30 12:45:28 +0100

    Updated logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from "../static/images/scottjohnston-dev-logo-flat-opt2.svg"
+// import logo from "../static/images/scottjohnston-dev-logo-flat-opt2.svg"
+import logo from "../static/images/logo.png"
 
 const Logo = () => {
     return (
